b139f5bd8dd849f7287ad4fe1c063c089499b42a
Beckham Scadlock <bscadlock02@gmail.com>
2024-02-07 14:51:19 -0500

    Create Page9.java

---
+++ Page9.java
@@ -0,0 +1,8 @@
+public class Page9
+{
+    public static void main (String[ ] args)
+{
+        System.out.println("Hi"); //message to user
+        System.out.println("LabOneA"); //assignment
+    }
+}
